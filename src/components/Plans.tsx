071aef83de954565701bea970141d901ac451c3a
Jesús David Julio Romero <140851329+11julio11@users.noreply.github.com>
2025-06-09 23:07:47 -0500

    nert.2

--- src/components/Plans.tsx
+++ src/components/Plans.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { useState } from 'react';
 import { Check, X, Loader2 } from 'lucide-react';
 import { createCheckoutSession } from '../lib/stripe';
